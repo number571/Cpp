750990e0cd7fd0fbdd8d2afbe2ca381ccf342540
[#571] <31502293+Number571@users.noreply.github.com>
2019-05-11 11:56:15 -0500

    Update rat-nums.cpp

--- rat-nums.cpp
+++ rat-nums.cpp
@@ -21,9 +21,12 @@ class rat {
         rat(int x) { numer = x; denom = 0; }
         rat() { numer = denom = 0; }
 
-        friend string get(rat x);
         int numers() { return numer; }
         int denoms() { return denom; }
+        friend ostream& operator<<(ostream &out, rat x) {
+            cout << to_string(x.numer) + "/" + to_string(x.denom);
+            return out;
+        }
 
         friend rat operator+(rat x, rat y) {
             int num = x.numer * y.denom + y.numer * x.denom;
@@ -58,14 +61,10 @@ class rat {
         friend rat operator/=(rat &x, rat y) { return x = x / y; }
 };
 
-string get(rat x) {
-    return to_string(x.numer) + "/" + to_string(x.denom);
-}
-
 int main(void) {
     rat x{4, 5};
     rat y{2, 4};
 
-    cout << get(x) + " + " + get(y) + " = " + get(x + y) << endl;
+    cout << x << " + " << y << " = " << x + y << endl;
     return 0;
 }
